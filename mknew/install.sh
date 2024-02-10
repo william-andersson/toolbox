c909e3cbcf82b568c355120f374e375fe603bb8a
William Andersson <william.andersson@outlook.com>
2024-02-10 15:18:55 +0100

    Add files via upload

---
+++ mknew/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# File for:    mknew (Github version)
+# Copyright:   William Andersson 2024
+# Website:     https://github.com/william-andersson
+# License:     GPL
+#
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+# Copy scripts
+install -v -C -m 775 -o root mknew.sh /usr/bin/toolbox-mknew
+echo "Done."
---
+++ mknew/mknew.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Application: mknew (Github version)
+# Copyright:   William Andersson 2024
+# Website:     https://github.com/william-andersson
+# License:     GPL
+#
+VERSION=3.3
+if [ ! $1 ];then
+	echo "Create new toolbox-script from template in current location."
+	echo -e "Usage: toolbox-create <NAME>\n"
+	exit 0
+fi
+echo "Creating directory $PWD/$1 ..."
+sleep .2
+mkdir $PWD/$1
+
+echo "Creating files ..."
+sleep .2
+touch $PWD/$1/BUGS
+touch $PWD/$1/NOTES
+touch $PWD/$1/CHANGELOG
+touch $PWD/$1/README
+touch $PWD/$1/build.cfg
+touch $PWD/$1/install.sh
+touch $PWD/$1/$1.sh
+
+echo "Configuring build.cfg ..."
+sleep .2
+echo "NAME=\"$1\"" >> $PWD/$1/build.cfg
+echo "COPY=\"William Andersson 2024\"" >> $PWD/$1/build.cfg
+echo "WEB=\"https://github.com/william-andersson\"" >> $PWD/$1/build.cfg
+echo "LICENSE=\"GPL\"" >> $PWD/$1/build.cfg
+sleep 1
+
+echo "Configuring install.sh ..."
+sleep .2
+echo "#!/bin/bash" >> $PWD/$1/install.sh
+echo "if [[ \$EUID -ne 0 ]]; then" >> $PWD/$1/install.sh
+echo "   echo \"This script must be run as root\"" >> $PWD/$1/install.sh
+echo "   exit 1" >> $PWD/$1/install.sh
+echo "fi" >> $PWD/$1/install.sh
+echo "" >> $PWD/$1/install.sh
+echo "DEPENDENCIES=(\"\")" >> $PWD/$1/install.sh
+echo "" >> $PWD/$1/install.sh
+echo "install -v -C -m 775 -o root $1.sh /usr/bin/toolbox-$1" >> $PWD/$1/install.sh
+echo "" >> $PWD/$1/install.sh
+echo "if [ ! -z \"\$DEPENDENCIES\" ];then" >> $PWD/$1/install.sh
+echo "    /usr/bin/toolbox-depin \${DEPENDENCIES[@]}" >> $PWD/$1/install.sh
+echo "fi" >> $PWD/$1/install.sh
+echo "" >> $PWD/$1/install.sh
+echo "echo \"Done.\"" >> $PWD/$1/install.sh
+
+
+echo "Configuring $1.sh ..."
+sleep .2
+echo "#!/bin/bash" >> $PWD/$1/$1.sh
+echo "VERSION=1.0" >> $PWD/$1/$1.sh
+
+echo "Done."
