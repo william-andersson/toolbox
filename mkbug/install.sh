353c4f3959c3aa9fd0077f92b3e7297b32118b22
William Andersson <william.andersson@outlook.com>
2024-03-24 15:10:05 +0100

    Add files via upload

--- mkbug/install.sh
+++ mkbug/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # File for:    mkbug (Github version)
+# Comment:     File new bug
 # Copyright:   William Andersson 2024
 # Website:     https://github.com/william-andersson
 # License:     GPL
@@ -12,7 +13,7 @@ fi
 
 DEPENDENCIES=("")
 
-install -v -C -m 775 -o root mkbug.sh /usr/bin/toolbox-mkbug
+install -v -C -m 775 -o root main.sh /usr/bin/toolbox-mkbug
 
 if [ ! -z "$DEPENDENCIES" ];then
     /usr/bin/toolbox-depin ${DEPENDENCIES[@]}
